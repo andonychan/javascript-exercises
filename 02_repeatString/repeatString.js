f73d9e5eef62c33c6e54bf783b7ef97300cf3274
Andy <andy_chan96@hotmail.com>
2022-02-23 05:41:17 +0800

    completed ex01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    let result = '';
+    while (n > 0) {
+        result += word;
+        n--;
+    }
+    while (n<0) {
+        return 'ERROR';
+    }
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let splitString = word.split(''); // => [h,e,l,l,o]
+    let reverseStringArray = splitString.reverse();//=> [o,l,l,e,h]
+    let joinStr = reverseStringArray.join('');
+    return joinStr;
 };
 
 // Do not edit below this line
