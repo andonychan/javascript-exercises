7f0313383d99f34c690dd889b368fc8fd5207ea1
Andy <andy_chan96@hotmail.com>
2022-02-28 01:41:46 +0800

    Completed up till 13.caesar.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let term = 0;
+    if (num == 0) {
+        term = 0;
+    }
+    else if (num == 1) {
+        term = 1;
+    }
+    else if (num < 0) {
+        return 'OOPS';
+    }
+    else {
+        term = fibonacci(num-1) + fibonacci(num-2);
+    }
+    return term;
 };
 
 // Do not edit below this line
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,31 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    return string
+    .split('') //convert string to array. eg. 'hello world!' returns ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!']
+    .map(char => charShift(char, shift)) //pass each array element through charShift function, to convert to code and shift as necessary.
+    .join('') //convert array back to string
+}
 
-};
+const codeSet = (code) => {return code < 97 ? 65 : 97};
+
+function mod(m ,n) {
+    return (m % n + n) % n;
+}
+
+function charShift(char, shift) {
+    const code = char.charCodeAt();
+
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+        return String.fromCharCode(mod(code + shift - codeSet(code), 26) + codeSet(code));
+    }
+
+    else {
+        return char;
+    }
+    
+}
 
 // Do not edit below this line
 module.exports = caesar;
+
+
+// A - 65; Z - 90; a - 97; z - 122
